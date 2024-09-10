fac9bcb4b2849b881a6bc5022f761944b08f50ad
EmilioCruzAs <emilioalejandrocs6@gmail.com>
2024-09-09 22:44:44 -0600

    upload content

---
+++ Botclient.cs
@@ -0,0 +1,45 @@
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Text;
+
+namespace logservice;
+
+class BotClient
+{
+    private string  message;
+    
+    public BotClient ( string _message)
+    {
+        message =_message??"";
+         
+
+    }
+
+    public async void Postasync()
+    {   
+        string token ="7341152621:AAGSDrs9anwxZ7pvR4S2ASX5qhl4Voj9zAM";
+        string url = $"https://api.telegram.org/bot{token}/sendMessage?chat_id={7226331689}&text={Uri.EscapeDataString(message)}";
+       
+        using(HttpClient client = new HttpClient())
+        {
+            try
+            {
+                var resquest = await client.GetAsync(url);
+                var responseContent = await resquest.Content.ReadAsStringAsync();
+                Console.WriteLine($"Respuesta del servidor: {responseContent}");
+
+                if(resquest.IsSuccessStatusCode){Console.WriteLine("Mensaje Enviado COrrectamente");}else{Console.WriteLine($"que paso {resquest.StatusCode}");};
+
+            }catch(HttpRequestException  e)
+            {
+                Console.WriteLine(e);
+            }catch(UriFormatException e)
+            {
+                Console.WriteLine(e);
+            }
+            
+        }
+        
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿
+using System;
+using System.Diagnostics.Eventing.Reader;
+namespace  logservice;
+class Program
+{
+    public static void Eventwatcher()
+    {
+        
+        string logquery= "Microsoft-Windows-TerminalServices-RemoteConnectionManager/Operational";
+        string queryId="*[System/EventID=1149]";
+        EventLogQuery query = new EventLogQuery(logquery,PathType.LogName, queryId);
+
+        Console.WriteLine("Esperando clientes");
+
+        using(EventLogWatcher watcher = new EventLogWatcher(query))
+        {
+            watcher.EventRecordWritten+= new EventHandler<EventRecordWrittenEventArgs>(OnEventRecordWritten);
+            watcher.Enabled = true;
+            Console.ReadKey();
+        }
+
+    }
+
+         
+     public static  void OnEventRecordWritten(object? sender, EventRecordWrittenEventArgs e)
+     {
+        if(e.EventRecord !=null)
+        {
+            object   user =  e.EventRecord.Properties[0].Value;
+            object ip = e.EventRecord.Properties[2].Value;
+            DateTime time = e.EventRecord.TimeCreated.Value;
+            string message= user.ToString() +" " +ip.ToString()+" " + time.ToString();
+            Console.WriteLine("EL CLIENTE SE HA CONECTADO");
+            BotClient cliente = new BotClient(message);
+            cliente.Postasync();
+        }
+
+     }
+
+        
+    
+ static void Main(string[] args)
+    {
+
+        Eventwatcher();
+        
+    }
+
+
+}
